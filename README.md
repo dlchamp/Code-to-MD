085766e8034c59521717e046c82d97785d629029
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-12-21 15:34:53 -0600

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# code-to-markdown README
+
+Copy your code snippets into Discord or Reddit ready formats. 
+
+## Features
+
+This extensions provides two features:
+- Copy to Discord |  Copy your selected code snippet into a Discord markdown ready format that you can quickly just paste into a Discord chat box
+- Copy to Reddit | Copy your selected code snippet into a Reddit markdown ready format that can be pasted right into your post or comment.
+
+## Version 0.0.1
+    - First working build with both features supported.
---
+++ extension.js
@@ -0,0 +1,50 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	vscode.commands.registerCommand('code-to-md.copyToDiscord', function () {
+		// Copy the snippet to the clipboard matching Discord's markdown formatting
+		// \`\`\` {language}
+		// 	# selected code
+		// \`\`\``
+
+		{
+			const editor = vscode.window.activeTextEditor;
+			const selection = editor.selection;
+			const text = editor.document.getText(selection);
+			const language = editor.document.languageId;
+			const formattedText = `\`\`\`${language}\n${text.trim()}\n\`\`\``;
+
+			vscode.env.clipboard.writeText(formattedText);
+		}
+	});
+
+	vscode.commands.registerCommand('code-to-md.copytoReddit', function () {
+		// Copy the selected snippet to the clipboard using Reddit's old style markdown formatting
+		// Where each line of code has affixed 4 extra spaces 
+
+		{
+			const editor = vscode.window.activeTextEditor;
+			const selection = editor.selection;
+			const text = editor.document.getText(selection);
+			const formattedText = text.trim().split('\n').map(line => '    ' + line).join('\n');
+
+			vscode.env.clipboard.writeText(formattedText);
+		}
+	});
+}
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
