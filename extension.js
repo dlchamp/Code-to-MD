31f395cff36114f3f345f7dd44a8912ff931a355
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2023-05-20 20:29:02 -0500

    fix error with copy to reddit not indenting first line properly

--- extension.js
+++ extension.js
@@ -1,12 +1,4 @@
 const vscode = require('vscode');
-const json5 = require('json5');
-
-function activate(context) {
-	vscode.commands.registerCommand('code-to-md.copyToDiscord', copyToDiscord);
-	vscode.commands.registerCommand('code-to-md.copyToReddit', copyToReddit);
-}
-
-function deactivate() { }
 
 async function getEditorSelection() {
 	const editor = vscode.window.activeTextEditor;
@@ -17,62 +9,44 @@ async function getEditorSelection() {
 	return [prettifiedText, language];
 }
 
-function removeInitialIndentation(text) {
-	const lines = text.split('\n');
-	const minIndentation = Math.min(
-		...lines
-			.filter(line => line.trim())
-			.map(line => line.match(/^\s*/)[0].length)
-	);
-
-	return lines
-		.map(line => line.slice(minIndentation))
-		.join('\n');
-}
-
-function copyToDiscord() {
-	getEditorSelection().then(([text, language]) => {
-		const leftAlignedText = removeInitialIndentation(text);
-		const formattedText = `\`\`\`${language}\n${leftAlignedText}\n\`\`\``;
-		vscode.env.clipboard.writeText(formattedText);
-	});
-}
-
-
-
-function copyToReddit() {
-	getEditorSelection().then(([text, _]) => {
-		const formattedText = text.split('\n').map(line => '  ' + line).join('\n');
-		vscode.env.clipboard.writeText(formattedText);
-	});
-}
-
-
 async function formatSelectedText(text, language) {
-	const untitledDocument = await vscode.workspace.openTextDocument({
-		content: text,
-		language,
-	});
-
-	const formattedDocument = await vscode.commands.executeCommand(
-		'editor.action.formatDocument',
-		untitledDocument.uri,
-	);
-
+	const untitledDocument = await vscode.workspace.openTextDocument({ content: text, language });
+	const formattedDocument = await vscode.commands.executeCommand('editor.action.formatDocument', untitledDocument.uri);
 	const formattedText = formattedDocument ? formattedDocument.getText() : text;
-
-	// Calculate the initial indentation of the first line
 	const initialIndentation = formattedText.match(/^\s*/)[0];
-
-	// Remove the initial indentation from all lines
-	const leftAlignedText = formattedText
-		.split('\n')
-		.map(line => line.replace(initialIndentation, ''))
-		.join('\n');
+	const leftAlignedText = formattedText.split('\n').map(line => line.replace(initialIndentation, '')).join('\n');
 
 	return leftAlignedText;
 }
 
+function removeInitialIndentation(text) {
+	const lines = text.split('\n');
+	const minIndentation = Math.min(...lines.filter(line => line.trim()).map(line => line.match(/^\s*/)[0].length));
+
+	return lines.map(line => line.slice(minIndentation)).join('\n');
+}
+
+async function copyToDiscord() {
+	const [text, language] = await getEditorSelection();
+	const leftAlignedText = removeInitialIndentation(text);
+	const formattedText = `\`\`\`${language}\n${leftAlignedText}\n\`\`\``;
+	await vscode.env.clipboard.writeText(formattedText);
+}
+
+async function copyToReddit() {
+	const [text, _] = await getEditorSelection();
+	const formattedText = text.split('\n').map(line => '    ' + line).join('\n');
+	await vscode.env.clipboard.writeText(formattedText);
+}
+
+function activate(context) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('code-to-md.copyToDiscord', copyToDiscord),
+		vscode.commands.registerCommand('code-to-md.copyToReddit', copyToReddit)
+	);
+}
+
+function deactivate() { }
 
 module.exports = {
 	activate,
