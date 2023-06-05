01bfbfde21df9a35081783935e078749304498db
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2023-06-04 19:58:58 -0500

    remove function to open new tab to format selected text

--- extension.js
+++ extension.js
@@ -1,23 +1,5 @@
 const vscode = require('vscode');
 
-async function getEditorSelection() {
-	const editor = vscode.window.activeTextEditor;
-	const selectedText = editor.document.getText(editor.selection);
-	const language = editor.document.languageId;
-	const prettifiedText = await formatSelectedText(selectedText, language);
-
-	return [prettifiedText, language];
-}
-
-async function formatSelectedText(text, language) {
-	const untitledDocument = await vscode.workspace.openTextDocument({ content: text, language });
-	const formattedDocument = await vscode.commands.executeCommand('editor.action.formatDocument', untitledDocument.uri);
-	const formattedText = formattedDocument ? formattedDocument.getText() : text;
-	const initialIndentation = formattedText.match(/^\s*/)[0];
-	const leftAlignedText = formattedText.split('\n').map(line => line.replace(initialIndentation, '')).join('\n');
-
-	return leftAlignedText;
-}
 
 function escapeBackticks(text) {
 	if (/```/.test(text)) {
@@ -29,22 +11,37 @@ function escapeBackticks(text) {
 function removeInitialIndentation(text) {
 	const lines = text.split('\n');
 	const minIndentation = Math.min(...lines.filter(line => line.trim()).map(line => line.match(/^\s*/)[0].length));
-
 	return lines.map(line => line.slice(minIndentation)).join('\n');
 }
 
+function formatSelectedText(text) {
+	const leftAlignedText = removeInitialIndentation(text)
+	const escapedText = escapeBackticks(leftAlignedText)
+
+	return escapedText;
+}
+
+
+async function getEditorSelection() {
+	const editor = vscode.window.activeTextEditor;
+	await vscode.commands.executeCommand('editor.action.formatDocument');
+	const selectedText = editor.document.getText(editor.selection);
+	const language = editor.document.languageId;
+	const prettifiedText = await formatSelectedText(selectedText, language);
+
+	return [prettifiedText, language];
+}
+
+
 async function copyToDiscord() {
 	const [text, language] = await getEditorSelection();
-	const escapedText = escapeBackticks(text)
-	const leftAlignedText = removeInitialIndentation(escapedText);
-	const formattedText = `\`\`\`${language}\n${leftAlignedText}\n\`\`\``;
+	const formattedText = `\`\`\`${language}\n${text}\n\`\`\``;
 	await vscode.env.clipboard.writeText(formattedText);
 }
 
 async function copyToReddit() {
 	const [text, _] = await getEditorSelection();
-	const leftAlignedText = removeInitialIndentation(text)
-	const formattedText = leftAlignedText.split('\n').map(line => '    ' + line).join('\n');
+	const formattedText = text.split('\n').map(line => '    ' + line).join('\n');
 	await vscode.env.clipboard.writeText(formattedText);
 }
 
